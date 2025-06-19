672063c4a585af541aa6fb0e66e535e96fd9fd87
Alexis Ruccius <a.ruccius@posteo.de>
2025-06-19 21:10:20 +0200

    Add max length validation for Generate Plant name field

--- lib/verdemind/botany/generate_plant.ex
+++ lib/verdemind/botany/generate_plant.ex
@@ -10,6 +10,7 @@ defmodule Verdemind.Botany.GeneratePlant do
     generate_plant
     |> cast(attrs, [:name])
     |> validate_required([:name])
-    |> validate_length(:name, min: 3)
+    # Most plant names are in the range of 3-60 characters.
+    |> validate_length(:name, min: 3, max: 60)
   end
 end
